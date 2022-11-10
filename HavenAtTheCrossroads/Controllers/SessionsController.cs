27bbd9f4c51f0d18f88f9bae2c2933c6d0123180
SullivanKE <102761752+SullivanKE@users.noreply.github.com>
2022-11-09 21:54:43 -0800

    Fixed sessionmodel view looking at charactermodel

--- HavenAtTheCrossroads/Controllers/SessionsController.cs
+++ HavenAtTheCrossroads/Controllers/SessionsController.cs
@@ -19,6 +19,40 @@ namespace HavenAtTheCrossroads.Controllers
         public IActionResult Edit()
         {
             SessionModel s = new();
+            s.SessionId = 1;
+            s.DM = "Katie";
+            s.Gold = 10;
+            s.Exp = 20;
+
+            CharacterModel c = new();
+            c.CharId = 1;
+            c.UserId = 1;
+            c.Name = "Eranir";
+            c.ConcurrencyId = 1;
+
+            SessionItemModel sim = new();
+            sim.SessionId = 1;
+            sim.Name = "Test data";
+            sim.Value = 1;
+            sim.Type = SessionItemModel.ItemType.Monster;
+
+            SessionItemModel sim2 = new();
+            sim2.SessionId = 1;
+            sim2.Name = "Test data";
+            sim2.Value = 1;
+            sim2.Type = SessionItemModel.ItemType.Adhoc;
+
+            SessionItemModel sim3 = new();
+            sim3.SessionId = 1;
+            sim3.Name = "Test data";
+            sim3.Value = 1;
+            sim3.Type = SessionItemModel.ItemType.Item;
+
+            s.Characters.Add(c);
+            s.Monsters.Add(sim);
+            s.AdHocs.Add(sim2);
+            s.Items.Add(sim3);
+
             return View(s);
         }
     }
--- HavenAtTheCrossroads/Models/SessionModel.cs
+++ HavenAtTheCrossroads/Models/SessionModel.cs
@@ -4,11 +4,11 @@
     {
         public int SessionId { get; set; }
         public string DM { get; set; } = "";
-        
-        public List<CharacterModel> ?Characters { get; set; }
-        public List<SessionItemModel> ?Monsters { get; set; }
-        public List<SessionItemModel> ?AdHocs { get; set; }
-        public List<SessionItemModel> ?Items { get; set; }
+
+        public List<CharacterModel> Characters { get; set; } = new();
+        public List<SessionItemModel> Monsters { get; set; } = new();
+        public List<SessionItemModel> AdHocs { get; set; } = new();
+        public List<SessionItemModel> Items { get; set; } = new();
 
         public int Gold { get; set; }
         public int Exp { get; set; }
